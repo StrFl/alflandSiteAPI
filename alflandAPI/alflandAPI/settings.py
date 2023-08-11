24b92e59b5f71070a4095e11efc0bbe74745c6a9
StrFl <ghgod123321@gmail.com>
2023-08-11 15:03:40 +0300

    my message asdasd

--- alflandAPI/alflandAPI/settings.py
+++ alflandAPI/alflandAPI/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rdl)y(krvv04$634x57!_op$*jbu6-kx3!t$n&=-7yi#@bt-lw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'rtde.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
